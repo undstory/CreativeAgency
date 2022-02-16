df1017b36122afca29dc9946d5cabd86d521c85b
undstory <undrul.ozog@gmail.com>
2022-02-16 13:09:33 +0100

    portfolio works

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const navToggler = document.querySelector(".nav__toggler");
     const mobileNav = document.querySelector(".mobile__nav");
-    const mobileClose = document.querySelector(".mobile__close");
+    const portfolioBtns = document.querySelectorAll('.portfolio__btn');
+    const portfolioProjects = document.querySelectorAll('.portfolio__project');
+
 
 
         navToggler.addEventListener('click', () => {
@@ -15,8 +17,25 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         })
 
+        portfolioBtns.forEach(btn => {
+            btn.addEventListener('click', function() {
+                portfolioBtns.forEach(btn => {
+                    btn.classList.remove('active');
+                })
+                this.classList.toggle('active');
+                const filterType = this.getAttribute('data-filter');
+                portfolioProjects.forEach(project => {
+                    project.style.display = 'none';
+                    if(project.getAttribute('data-filter') === 'all' || filterType === 'all' || project.getAttribute('data-filter') === filterType) {
+                        project.style.display = 'block';
+                    }
+                })
+            })
+        })
+
 
 
 
 
 })
+
